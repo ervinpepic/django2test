7b81dd1fe2dfa2b13ce1b66498dcd2f9d162d961
Ervin Pepic <ervin.hack@gmail.com>
2018-11-21 23:59:08 +0100

    Updating models and models fields

--- django2test/products/models.py
+++ django2test/products/models.py
@@ -6,7 +6,8 @@ class Product(models.Model):
 	title 		= models.CharField(max_length=70)
 	description = models.TextField()
 	price 		= models.DecimalField(decimal_places=2, max_digits=10000)
-	summary 	= models.TextField(blank=True, null=True)
+	summary 	= models.TextField(blank=False, null=False)
+	featured	= models.BooleanField()
 
 	def __str__(self):
 		return self.title
