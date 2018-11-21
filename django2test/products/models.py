87c5255a751c0073190495e489deaf50fa7a12f8
Ervin Pepic <ervin.hack@gmail.com>
2018-11-21 23:46:13 +0100

    Change models.py(updated)

--- django2test/products/models.py
+++ django2test/products/models.py
@@ -3,7 +3,10 @@ from django.db import models
 # Create your models here.
 
 class Product(models.Model):
-	title 		= models.TextField()
+	title 		= models.CharField(max_length=70)
 	description = models.TextField()
-	price 		= models.TextField()
-	summary 	= models.TextField(default='This is cool')
+	price 		= models.DecimalField(decimal_places=2, max_digits=10000)
+	summary 	= models.TextField(blank=True, null=True)
+
+	def __str__(self):
+		return self.title
